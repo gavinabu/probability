752ce1796875767ce32612812738fba9254d5e3a
Gavin Abu-Zahra <gavinabuzahra@justwhatever.net>
2024-06-20 15:27:14 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import './app.css'
+import PlusIcon from './plus.svg'
+import React, { useEffect, useState } from "react";
+import Pair from './pair'
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+function App() {
+  var searchParams = new URLSearchParams(window.location.search)
+  let [pairs,setPairs] = useState(searchParams.has("p") ? JSON.parse(window.atob(searchParams.get("p"))) : [
+    ["number",0,4],
+    ["letter","A","D"]
+  ])
+  function generateRandomList(type,min,max) {
+    var returni = []
+    if(type == "number") {
+      for (let index = min; index <= max; index++) {
+        returni.push(index)
+      }
+    }
+    if(type == "letter") {
+      for (let index = letters.indexOf(min); index <= letters.indexOf(max); index++) {
+        returni.push(letters[index])
+      }
+    }
+    return returni
+  }
+  function getRandomInList(list) {
+    return list[Math.floor(Math.random() * list.length)]
+  }
+  function generateCombo() {
+    var rs = ''
+    pairs.forEach(pair => {
+      rs += getRandomInList(generateRandomList(pair[0],pair[1],pair[2]))
+    })
+    return rs
+  }
+  function repeatStr(str,num) {
+    var nstr = ''
+    for (let index = 0; index < num; index++) {
+      nstr += str
+    }
+    return nstr
+  }
+  var [combo,setCombo] = useState(generateCombo())
+  var [ebtn,setEbtn] = useState("Export")
+  return (
+    <div className="application">
+      <p>Random Combo: {combo}</p>
+      <div className="btns">
+        <button onClick={() => {
+        setCombo(repeatStr("_",pairs.length))
+        setTimeout(() => {
+          setCombo(generateCombo)
+        },400)
+        
+      }}>Generate Combo</button>
+      <button onClick={() => {
+        navigator.clipboard.writeText(`https://prob.justwhatever.net/?p=${window.btoa(JSON.stringify(pairs))}`);
+        setEbtn("Copied")
+        setTimeout(() => {
+          setEbtn("Export")
+        },1500)
+        
+      }}>{ebtn}</button>
+      </div>
+      
+      <h4>Pairs</h4>
+      <div className="pairs">
+        {pairs.map((p,index) => (
+          <Pair key={index} defaulttype={p[0]} defaultmin={p[1]} defaultmax={p[2]} onUpdate={(type,min,max) => {
+            p[0] = type
+            p[1] = min
+            p[2] = max
+          }}/>
+        ))}
+        <button style={{background:"none",border:"none",cursor:"pointer"}} onClick={() => {
+          setPairs([...pairs, ["number", 0, 9]])
+        }}>
+          <div className="card" style={{alignItems:"center"}}>
+            <img src={PlusIcon} style={{scale:"40%"}}/>
+          </div>
+        </button>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pair.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react"
+import Slider from 'multi-range-slider-react'
+import './card.css'
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+export default function Pair(params) {
+  var [min,setMin] = useState(params.defaultmin ? params.defaultmin : 0)
+  var [type,setType] = useState(params.defaulttype ? params.defaulttype : "number")
+  var [max,setMax] = useState(params.defaultmax ? params.defaultmax : 9)
+  return (
+    <div className="card">
+        <select defaultValue={type} onChange={(e) => {
+          setType(e.target.value)
+          params.onUpdate(e.target.value,min,max)
+        }}>
+          <option value="number">Number</option>
+          <option value="letter">Letter</option>
+        </select>
+        {type == "number" ? (
+          <Slider id='number' minValue={params.defaultmin ? params.defaultmin : 0} maxValue={params.defaultmax ? params.defaultmax : 9} label="none" ruler="none" style={{border:"none",boxShadow:"none",padding:"none",width:'150px'}} step="1" min="0" onInput={(e) => {
+            setMin(e.minValue)
+            setMax(e.maxValue)
+            params.onUpdate(type,e.minValue,e.maxValue)
+          }} max="9" barLeftColor="#ccccac" barInnerColor="#337aff" barRightColor="#ccccac"  />
+        ) : (
+          <>
+            <Slider id="letter" min="0" max="25" labels={letters} label="none" ruler="none" style={{border:"none",boxShadow:"none",padding:"none",width:'150px'}} step="1" minValue="0" onInput={(e) => {
+              setMin(letters[e.minValue])
+              setMax(letters[e.maxValue])
+              params.onUpdate(type,letters[e.minValue],letters[e.maxValue])
+            }} maxValue="3" barLeftColor="#ccccac" barInnerColor="#337aff" barRightColor="#ccccac"  />
+          </>
+        )}
+        <p>Selected range: {min} - {max}</p>
+    </div>
+  ) 
+}
